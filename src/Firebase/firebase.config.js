7ee4b063c8ee40020795a293f7a85db1a0f71d4c
Saif Uddin Ahmed <neononezero@gmail.com>
2021-10-16 17:26:41 +0600

    Added firebase in ema john simple

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7OzX-P8NgauZRcATYoLj2rTrA5LLaABk",
+    authDomain: "ema-john-simple59.firebaseapp.com",
+    projectId: "ema-john-simple59",
+    storageBucket: "ema-john-simple59.appspot.com",
+    messagingSenderId: "871629845018",
+    appId: "1:871629845018:web:64b2f184dca2ed02f8d7a3"
+  };
+
+  export default firebaseConfig;
